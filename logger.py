71f21533ddab60a12741b0e2810aab73d4856d0d
basakamari10-hash <basakamari.10@gmail.com>
2026-08-01 07:34:58 +0700

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,21 @@
+import sys
+import logging
+
+def setup_logger() -> logging.Logger:
+    """Configures structured logging."""
+    logger = logging.getLogger("IslamicAI")
+    logger.setLevel(logging.INFO)
+    
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter(
+        fmt="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    handler.setFormatter(formatter)
+    
+    if not logger.handlers:
+        logger.addHandler(handler)
+        
+    return logger
+
+LOGGER = setup_logger()
